0cf7da1780ef412cd4a11808d2ae83af16569a36
Kingsley Nnaji <kingsley.nnaji@gmail.com>
2019-01-05 02:06:28 +0100

    jscripts task created including gulp plugin installation and .babelrc file

---
+++ gulpfile.babel.js
@@ -0,0 +1,25 @@
+import gulp from 'gulp';
+import babelify from 'babelify';
+import browserify from 'browserify';
+import buffer from 'vinyl-buffer';
+import sourcemaps from 'gulp-sourcemaps';
+import uglify from 'gulp-uglify';
+import source from 'vinyl-source-stream';
+
+let jscripts = function() {
+    console.log('Running JS Tasks');
+    return (
+        browserify({
+            entries: ['./src/scripts/js/main.js']
+        })
+        .transform(babelify)
+        .bundle()
+        .pipe(source('bundle.min.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./dist/assets/js')));
+}
+
+export { jscripts };
