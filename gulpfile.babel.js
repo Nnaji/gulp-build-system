b7f5a11c18d7de6aa8446abc827289fc69313123
Kingsley Nnaji <kingsley.nnaji@gmail.com>
2019-10-30 18:21:33 +0100

    add postcss and implemented linting features such as prettier stylelint

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,111 +1,117 @@
-import gulp from "gulp";
-import babelify from "babelify";
-import browserify from "browserify";
-import buffer from "vinyl-buffer";
-import sourcemaps from "gulp-sourcemaps";
-import uglify from "gulp-uglify";
-import source from "vinyl-source-stream";
-import eslint from "gulp-eslint";
-import sass from "gulp-sass";
-import postcss from "gulp-postcss";
-import autoprefixer from "gulp-autoprefixer";
-import imagemin from "gulp-imagemin";
-import cleanCss from "gulp-clean-css";
-import fs from "fs";
-import del from "del";
-import print from "gulp-print";
-import browserSync from "browser-sync";
+import gulp from 'gulp';
+import babelify from 'babelify';
+import browserify from 'browserify';
+import buffer from 'vinyl-buffer';
+import sourcemaps from 'gulp-sourcemaps';
+import uglify from 'gulp-uglify';
+import source from 'vinyl-source-stream';
+import eslint from 'gulp-eslint';
+import sass from 'gulp-sass';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+import imagemin from 'gulp-imagemin';
+import cleanCss from 'gulp-clean-css';
+import stylelint from 'stylelint';
+import fs from 'fs';
+import del from 'del';
+import print from 'gulp-print';
+import browserSync from 'browser-sync';
 
 // Local imports
-import { paths } from "./gutils/paths";
-import { logMessages } from "./gutils/log-messages";
+import { paths } from './gutils/paths';
+import { logMessages } from './gutils/log-messages';
 
 browserSync.create();
 
 // Dev Server with browsersync
 let serve = function() {
-  logMessages("Running Serve Task");
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
-  wFiles();
+    logMessages('Running Serve Task');
+    browserSync.init({
+        server: {
+            baseDir: './',
+        },
+    });
+    wFiles();
 };
 
 // Javascript Task section f()
 let jscripts = function() {
-  logMessages("Running JS Tasks");
-  lint();
-  return browserify({ entries: [paths.jscripts.src] })
-    .transform(babelify)
-    .bundle()
-    .pipe(source("bundle.min.js"))
-    .pipe(buffer())
-    .pipe(sourcemaps.init())
-    .pipe(uglify())
-    .pipe(sourcemaps.write("./maps"))
-    .pipe(gulp.dest("./dist/assets/js"))
-    .pipe(browserSync.stream());
+    logMessages('Running JS Tasks');
+    lint();
+    return browserify({ entries: [paths.jscripts.src] })
+        .transform(babelify)
+        .bundle()
+        .pipe(source('bundle.min.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./dist/assets/js'))
+        .pipe(browserSync.stream());
 };
 
 // JS linting Task section f()
 let lint = function() {
-  logMessages("Running Linting Task");
-  logMessages("Linting JS files");
-  return gulp
-    .src(["./src/scripts/js/*.js", "./*js", "!./node_modules/**/*.js"])
-    .pipe(print())
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-};
-
-// Styles Task section f()
-let styles = function() {
-  logMessages("Running Styles task");
-  let processors = [autoprefixer];
-  return gulp
-    .src(paths.styles.src)
-    .pipe(print())
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(postcss(processors))
-    .pipe(cleanCss())
-    .pipe(sourcemaps.write("./map"))
-    .pipe(gulp.dest(paths.styles.dest))
-    .pipe(browserSync.stream());
+    logMessages('Running Linting Task');
+    logMessages('Linting JS files');
+    return gulp
+        .src(['./src/scripts/js/*.js', './*js', '!./node_modules/**/*.js'])
+        .pipe(print())
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 };
 
 // Clean Task Section
 let clean = function(cb) {
-  logMessages("Runnig Cleaning Task");
-  if (!fs.existsSync("dist")) {
-    logMessages("dist folder does not exists");
-  } else {
-    logMessages("Deleting dist folder........");
-    del.sync("dist");
-    logMessages("dist folder deleted");
-  }
-  cb();
+    logMessages('Runnig Cleaning Task');
+    if (!fs.existsSync('dist')) {
+        logMessages('dist folder does not exists');
+    } else {
+        logMessages('Deleting dist folder........');
+        del.sync('dist');
+        logMessages('dist folder deleted');
+    }
+    cb();
+};
+
+// Styles Task section f()
+let styles = function() {
+    logMessages('Running Styles task');
+    let processors = [
+        autoprefixer(),
+        stylelint({
+            reporters: [{ formatter: 'string', fix: true }],
+        }),
+    ];
+    return gulp
+        .src(paths.styles.src)
+        .pipe(print())
+        .pipe(sourcemaps.init())
+        .pipe(postcss(processors))
+        .pipe(sass())
+        .pipe(cleanCss())
+        .pipe(sourcemaps.write('./map'))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browserSync.stream());
 };
 
 // Image Task Section
 let image = function() {
-  logMessages("Running Image Task");
-  return gulp
-    .src(paths.images.src)
-    .pipe(print())
-    .pipe(imagemin({ verbose: true }))
-    .pipe(gulp.dest(paths.images.dest))
-    .pipe(browserSync.stream());
+    logMessages('Running Image Task');
+    return gulp
+        .src(paths.images.src)
+        .pipe(print())
+        .pipe(imagemin({ verbose: true }))
+        .pipe(gulp.dest(paths.images.dest))
+        .pipe(browserSync.stream());
 };
 
 let wFiles = () => {
-  logMessages("Running Watch Files Task");
-  gulp.watch("./src/scripts/**/*.js", jscripts);
-  gulp.watch("./src/styles/**/*.scss", styles);
-  gulp.watch("./*.html").on("change", browserSync.reload);
+    logMessages('Running Watch Files Task');
+    gulp.watch('./src/scripts/**/*.js', jscripts);
+    gulp.watch('./src/styles/**/*.scss', styles);
+    gulp.watch('./*.html').on('change', browserSync.reload);
 };
 
 const build = gulp.series(clean, gulp.parallel(styles, jscripts, image, serve));
